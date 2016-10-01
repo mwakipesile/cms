322e4ea6aeed493d74b998a11f18481dd32fb7a3
M. Edgar joel <mkakajoel@gmail.com>
2016-10-01 10:18:16 -0400

    Added file editing

--- cms.rb
+++ cms.rb
@@ -4,21 +4,37 @@ require 'sinatra/content_for'
 require 'tilt/erubis'
 require 'redcarpet'
 
+configure do
+  enable :sessions
+  set :session_secret, 'password1'
+  set :erb, :escape_html => true
+end
+
 root = File.expand_path("..", __FILE__)
 
 before do
-  @flash_message = {}  
+  headers["Content-Type"] = "text/html;charset=utf-8"
 end
 
 helpers do
-  def load_content(text)
+  def render_markdown(text)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     markdown.render(text)
+  end
+
+  def load_file_content(path)
+    content = File.read(path)
+    case File.extname(path)
+    when ".txt"
+      headers["Content-Type"] = "text/plain"
+      content
+    when ".md"
+      render_markdown(content)
+    end
   end
 end
 
 get "/" do
-  headers["Content-Type"] = "text/html;charset=utf-8"
   @filenames = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
@@ -26,20 +42,29 @@ get "/" do
   erb :index
 end
 
+get '/:filename/edit' do |filename|
+  @filename = filename
+  @file_content = load_file_content("#{root}/data/#{filename}")
+  headers["Content-Type"] = "text/html;charset=utf-8"
+
+  erb :edit_file
+end
+
 get "/*.*" do |filename, ext|
   filepath = "#{root}/data/#{filename}.#{ext}"
-  
-  unless File.exist?(filepath)
-    @flash_message[:not_found] = "#{filename}.#{ext} does not exist."
+
+  if File.exist?(filepath)
+    load_file_content(filepath)
+  else
+    session[:message] = "#{filename}.#{ext} does not exist."
     redirect('/')
   end
+end
 
-  text = File.read(filepath)
-  if ext == 'md'
-    headers['Content-Type'] = "text/html;charset=utf-8"
-    load_content(text)
-  else
-    headers['Content-Type'] = "text/plain"
-    text
-  end
+post '/:filename' do |filename|
+  filepath = "#{root}/data/#{filename}"
+  text = params[:file_content]
+  File.open(filepath, "w") { |file| file.write(text) }
+
+  redirect("/#{filename}")
 end
