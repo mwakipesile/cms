9b7d99764a02ac5a20d82b69f942cab414466cdb
M. Edgar joel <mkakajoel@gmail.com>
2016-10-03 09:47:55 -0400

    Rubocop made me do it!

--- cms.rb
+++ cms.rb
@@ -7,30 +7,30 @@ require 'redcarpet'
 configure do
   enable :sessions
   set :session_secret, 'password1'
-  set :erb, :escape_html => true
+  set :erb, escape_html: true
 end
 
 def data_path
-  if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/data", __FILE__)
+  if ENV['RACK_ENV'] == 'test'
+    File.expand_path('../test/data', __FILE__)
   else
-    File.expand_path("../data", __FILE__)
+    File.expand_path('../data', __FILE__)
   end
 end
 
-def create_document(name, content = "")
-  File.open(File.join(data_path, name), "w") do |file|
+def create_document(name, content = '')
+  File.open(File.join(data_path, name), 'w') do |file|
     file.write(content)
   end
 end
 
 before do
-  headers["Content-Type"] = "text/html;charset=utf-8"
+  headers['Content-Type'] = 'text/html;charset=utf-8'
 end
 
 helpers do
   def file_list
-    Dir.glob(File.join(data_path, "*")).map { |path| File.basename(path) }
+    Dir.glob(File.join(data_path, '*')).map { |path| File.basename(path) }
   end
 
   def render_markdown(text)
@@ -41,17 +41,17 @@ helpers do
   def load_file_content(path)
     content = File.read(path)
     case File.extname(path)
-    when ".txt"
-      headers["Content-Type"] = "text/plain"
+    when '.txt'
+      headers['Content-Type'] = 'text/plain'
       content
-    when ".md"
+    when '.md'
       @content = render_markdown(content)
       erb :file
     end
   end
 end
 
-get "/" do
+get '/' do
   @filenames = file_list
 
   erb :index
@@ -60,12 +60,12 @@ end
 get '/:filename/edit' do |filename|
   @filename = filename
   @file_content = File.read(File.join(data_path, filename))
-  headers["Content-Type"] = "text/html;charset=utf-8"
+  headers['Content-Type'] = 'text/html;charset=utf-8'
 
   erb :edit_file
 end
 
-get "/*.*" do |filename, ext|
+get '/*.*' do |filename, ext|
   filepath = "#{data_path}/#{filename}.#{ext}"
 
   if File.exist?(filepath)
@@ -80,10 +80,10 @@ post '/:filename' do |filename|
   filepath = File.join(data_path, filename)
   text = params[:file_content]
 
-  File.open(filepath, "w") { |file| file.write(text) }
+  File.open(filepath, 'w') { |file| file.write(text) }
 
   session[:message] = "#{filename} has been updated!"
-  redirect("/")
+  redirect('/')
 end
 
 get '/new' do
@@ -96,14 +96,13 @@ post '/files/create' do
   if !filename.match(/\w+\.\w{2,}/)
     session[:message] = 'A valid name is required'
   elsif file_list.include?(filename)
-    session[:message] = "A document with name #{filename} already exists"
+    session[:message] = 'A document with name #{filename} already exists'
   else
     create_document(filename)
     session[:message] = "#{filename} was created"
-    redirect('/')    
+    redirect('/')
   end
 
   status(422)
   erb :new_file
 end
-
